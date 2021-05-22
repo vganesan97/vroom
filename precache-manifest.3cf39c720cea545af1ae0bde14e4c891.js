bb391cc6f9fb68ee69c8be8226c31027b55d0297
vishaal <vishaalganesan@vishaals-MacBook-Air.local>
2021-05-21 17:39:17 -0700

    Updates

---
+++ precache-manifest.3cf39c720cea545af1ae0bde14e4c891.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfe1ae2d9533f02817d0313548c814b4",
+    "url": "/vroom/index.html"
+  },
+  {
+    "revision": "1d9ab15bc8dfdab34d00",
+    "url": "/vroom/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2b5f054842ed94a00372",
+    "url": "/vroom/static/css/main.5ca3bf08.chunk.css"
+  },
+  {
+    "revision": "1d9ab15bc8dfdab34d00",
+    "url": "/vroom/static/js/2.e000a12a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/vroom/static/js/2.e000a12a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b5f054842ed94a00372",
+    "url": "/vroom/static/js/main.d27d093d.chunk.js"
+  },
+  {
+    "revision": "155b1b8bd123f23574e2",
+    "url": "/vroom/static/js/runtime-main.a319dbda.js"
+  }
+]);
